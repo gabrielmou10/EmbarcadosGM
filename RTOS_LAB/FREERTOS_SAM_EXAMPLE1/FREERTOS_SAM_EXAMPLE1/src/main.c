8708133dec71250353748285795cb098a35aa2af
Gabrielmou <gabrielmoura98@gmail.com>
2019-05-13 14:49:50 -0300

    LABS

---
+++ RTOS_LAB/FREERTOS_SAM_EXAMPLE1/FREERTOS_SAM_EXAMPLE1/src/main.c
@@ -0,0 +1,323 @@
+/**
+ * \file
+ *
+ * \brief FreeRTOS configuration
+ *
+ * Copyright (c) 2012-2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Subject to your compliance with these terms, you may use Microchip
+ * software and any derivatives exclusively with Microchip products.
+ * It is your responsibility to comply with third party license terms applicable
+ * to your use of third party software (including open source software) that
+ * may accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+ * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+ * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+ * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+ * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+ * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * \asf_license_stop
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+ 
+/**
+ * \mainpage FreeRTOS Real Time Kernel example
+ *
+ * \section Purpose
+ *
+ * The FreeRTOS example will help users how to use FreeRTOS in SAM boards.
+ * This basic application shows how to create task and get information of
+ * created task.
+ *
+ * \section Requirements
+ *
+ * This package can be used with SAM boards.
+ *
+ * \section Description
+ *
+ * The demonstration program create two task, one is make LED on the board
+ * blink at a fixed rate, and another is monitor status of task.
+ *
+ * \section Usage
+ *
+ * -# Build the program and download it inside the evaluation board. Please
+ *    refer to the
+ *    <a href="http://www.atmel.com/dyn/resources/prod_documents/doc6224.pdf">
+ *    SAM-BA User Guide</a>, the
+ *    <a href="http://www.atmel.com/dyn/resources/prod_documents/doc6310.pdf">
+ *    GNU-Based Software Development</a>
+ *    application note or to the
+ *    <a href="ftp://ftp.iar.se/WWWfiles/arm/Guides/EWARM_UserGuide.ENU.pdf">
+ *    IAR EWARM User Guide</a>,
+ *    depending on your chosen solution.
+ * -# On the computer, open and configure a terminal application
+ *    (e.g. HyperTerminal on Microsoft Windows) with these settings:
+ *   - 115200 bauds
+ *   - 8 bits of data
+ *   - No parity
+ *   - 1 stop bit
+ *   - No flow control
+ * -# Start the application.
+ * -# LED should start blinking on the board. In the terminal window, the
+ *    following text should appear (values depend on the board and chip used):
+ *    \code
+	-- Freertos Example xxx --
+	-- xxxxxx-xx
+	-- Compiled: xxx xx xxxx xx:xx:xx --
+\endcode
+ *
+ */
+
+#include <asf.h>
+#include "conf_board.h"
+
+#define TASK_MONITOR_STACK_SIZE            (2048/sizeof(portSTACK_TYPE))
+#define TASK_MONITOR_STACK_PRIORITY        (tskIDLE_PRIORITY)
+#define TASK_LED_STACK_SIZE                (1024/sizeof(portSTACK_TYPE))
+#define TASK_LED_STACK_PRIORITY            (configMAX_PRIORITIES - 1)
+
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
+		signed char *pcTaskName);
+extern void vApplicationIdleHook(void);
+extern void vApplicationTickHook(void);
+extern void vApplicationMallocFailedHook(void);
+extern void xPortSysTickHandler(void);
+
+
+
+ #define BUT_PIO PIOA
+ #define BUT_PIO_ID 10
+ #define BUT_PIO_IDX 11u
+ #define BUT_IDX_MASK (1u << BUT_PIO_IDX)
+
+
+
+/**
+ * \brief Called if stack overflow during execution
+ */
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
+		signed char *pcTaskName)
+{
+	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
+	/* If the parameters have been corrupted then inspect pxCurrentTCB to
+	 * identify which task has overflowed its stack.
+	 */
+	for (;;) {
+	}
+}
+
+/**
+ * \brief This function is called by FreeRTOS idle task
+ */
+extern void vApplicationIdleHook(void)
+{
+	pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+}
+
+/**
+ * \brief This function is called by FreeRTOS each tick
+ */
+extern void vApplicationTickHook(void)
+{
+}
+
+extern void vApplicationMallocFailedHook(void)
+{
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+
+	/* Force an assert. */
+	configASSERT( ( volatile void * ) NULL );
+}
+
+/**
+ * \brief This task, when activated, send every ten seconds on debug UART
+ * the whole report of free heap and total tasks status
+ */
+/** Semaforo a ser usado pela task led */
+SemaphoreHandle_t xSemaphore;
+
+/**
+ * callback do botao
+ * libera semaforo: xSemaphore
+ */
+void but_callback(void){
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	printf("but_callback \n");
+	xSemaphoreGiveFromISR(xSemaphore, &xHigherPriorityTaskWoken);
+	printf("semafaro tx \n");
+}
+
+// Inicializa botao SW0 do kit com interrupcao
+void io_init(void)
+{
+  // Inicializa clock do periférico PIO responsavel pelo botao
+  pmc_enable_periph_clk(BUT_PIO_ID);
+
+  // Configura PIO para lidar com o pino do botão como entrada
+  // com pull-up
+  pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+
+  // Configura interrupção no pino referente ao botao e associa
+  // função de callback caso uma interrupção for gerada
+  // a função de callback é a: but_callback()
+  
+  pio_handler_set(BUT_PIO,
+  BUT_PIO_ID,
+  BUT_IDX_MASK,
+  PIO_IT_FALL_EDGE,
+  but_callback);
+
+  // Ativa interrupção
+  pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
+
+  // Configura NVIC para receber interrupcoes do PIO do botao
+  // com prioridade 4 (quanto mais próximo de 0 maior)
+  NVIC_EnableIRQ(BUT_PIO_ID);
+  NVIC_SetPriority(BUT_PIO_ID, 4); // Prioridade 4
+}
+
+
+
+
+
+
+
+static void task_monitor(void *pvParameters)
+{
+	static portCHAR szList[256];
+	UNUSED(pvParameters);
+	const TickType_t xDelay = 1000/ portTICK_PERIOD_MS;
+	for (;;) {
+		printf("--- task ## %u", (unsigned int)uxTaskGetNumberOfTasks());
+		vTaskList((signed portCHAR *)szList);
+		printf(szList);
+		vTaskDelay(xDelay);
+	}
+}
+
+/**
+ * \brief This task, when activated, make LED blink at a fixed rate
+ */
+/**
+static void task_led(void *pvParameters)
+{
+	const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+	for (;;) {
+		LED_Toggle(LED0);
+		vTaskDelay(xDelay);
+	}
+}
+**/
+
+static void task_led(void *pvParameters)
+{
+        /* We are using the semaphore for synchronisation so we create a binary
+        semaphore rather than a mutex.  We must make sure that the interrupt
+        does not attempt to use the semaphore before it is created! */
+	xSemaphore = xSemaphoreCreateBinary();
+
+        /* devemos iniciar a interrupcao no pino somente apos termos alocado
+           os recursos (no caso semaforo), nessa funcao inicializamos 
+           o botao e seu callback*/
+        io_init();
+
+	if (xSemaphore == NULL)
+		printf("falha em criar o semaforo \n");
+
+	for (;;) {
+		if( xSemaphoreTake(xSemaphore, ( TickType_t ) 500) == pdTRUE ){
+			const TickType_t xDelay = 100/ portTICK_PERIOD_MS;
+			
+			for(int i =0; i<5;i++){
+				LED_Toggle(LED0);
+				vTaskDelay( xDelay );
+			}
+			
+		}
+	}
+}
+/**
+ * \brief Configure the console UART.
+ */
+static void configure_console(void)
+{
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = CONF_UART_BAUDRATE,
+#if (defined CONF_UART_CHAR_LENGTH)
+		.charlength = CONF_UART_CHAR_LENGTH,
+#endif
+		.paritytype = CONF_UART_PARITY,
+#if (defined CONF_UART_STOP_BITS)
+		.stopbits = CONF_UART_STOP_BITS,
+#endif
+	};
+
+	/* Configure console UART. */
+	stdio_serial_init(CONF_UART, &uart_serial_options);
+
+	/* Specify that stdout should not be buffered. */
+#if defined(__GNUC__)
+	setbuf(stdout, NULL);
+#else
+	/* Already the case in IAR's Normal DLIB default configuration: printf()
+	 * emits one character at a time.
+	 */
+#endif
+}
+
+/**
+ *  \brief FreeRTOS Real Time Kernel example entry point.
+ *
+ *  \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+	/* Initialize the SAM system */
+	sysclk_init();
+	board_init();
+
+	/* Initialize the console uart */
+	configure_console();
+
+	/* Output demo information. */
+	printf("-- Freertos Example --\n\r");
+	printf("-- %s\n\r", BOARD_NAME);
+	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+
+
+	/* Create task to monitor processor activity */
+	if (xTaskCreate(task_monitor, "Monitor", TASK_MONITOR_STACK_SIZE, NULL,
+			TASK_MONITOR_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create Monitor task\r\n");
+	}
+
+	/* Create task to make led blink */
+	if (xTaskCreate(task_led, "Led", TASK_LED_STACK_SIZE, NULL,
+			TASK_LED_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test led task\r\n");
+	}
+
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+
+	/* Will only get here if there was insufficient memory to create the idle task. */
+	return 0;
+}
